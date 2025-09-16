a52f1e9c07e74efa6a6991f7658837c17f30b886
ramya <ramyasrichintalapudi14@gmail.com>
2025-09-16 21:50:12 +0530

    Learning Java

---
+++ src/Ex_001_JavaBasics/Lab001_HelloWorld.java
@@ -0,0 +1,9 @@
+package Ex_001_JavaBasics;
+
+import java.sql.SQLOutput;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
