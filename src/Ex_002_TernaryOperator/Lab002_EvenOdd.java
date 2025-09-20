1e5ed4dc13aee4814261f3c597e89dbe7f4e8e7c
ramya <ramyasrichintalapudi14@gmail.com>
2025-09-20 20:01:19 +0530

    Ternary Operator EX-1

---
+++ src/Ex_002_TernaryOperator/Lab002_EvenOdd.java
@@ -0,0 +1,8 @@
+package Ex_002_TernaryOperator;
+
+public class Lab002_EvenOdd {
+    public static void main(String[] args) {
+        int num = 234568;
+        System.out.println((num%2==0)?"Even":"Odd");
+    }
+}
